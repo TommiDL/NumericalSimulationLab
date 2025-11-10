72f17d7e375271e5d1a2f41d9c09e683ebbb9ed1
TommiDL <tommaso.diluciano@studenti.unimi.it>
2025-11-10 12:01:44 +0100

    temporary main

--- es9/es9.0.cpp
+++ es9/es9.0.cpp
@@ -1,6 +1,7 @@
 #include "function.h"
 #include "map.h"
 #include "population.h"
+#include <string>
 
 using namespace std;
 
@@ -12,32 +13,67 @@ int main()
     pop.print();
 */
 
-    int ncities=10, dimpop=100;
+    int ncities=34, dimpop=100;
 
-    coordinates coord(ncities, "circle");
+    string path="square";
+    // coordinates coord(ncities, path);
+    string coord_file="coordinates.csv";
+    coordinates coord(coord_file);
 
     coord.print();
     cout << "---end of coord --\n";
-    coord.save_coordinates("coordinates.csv");
 
     population pop(coord, dimpop);
     //pop.print();
 
-    pop.set_mutation_probability(
-        0.8, 
-        0.8, 
-        0.8
+
+    ifstream read ("seed.in");
+
+    string seed;
+    getline(read, seed);
+
+    // Using the erase, remove_if, and ::isspace functions.
+    seed.erase(remove_if(seed.begin(),
+     seed.end(), ::isspace),
+        seed.end());
+    cout << "seed = " << stoi(seed) <<"\n";
+
+    pop.set_best_filename(
+        "OUTPUT/best_sol_"
+        +path+"_seed"
+        + to_string(stoi(seed))+".dat"
+    );
+    //coord.save_coordinates("coordinates.csv");
+
+
+    pop.set_avg_filename(
+        "OUTPUT/avg_"
+        +path+"_seed"
+        + to_string(stoi(seed))+".dat"
     );
 
 
+    pop.initialize_files();
 
-    int nsteps=10000;
+    pop.set_mutation_probability(
+        0.1, 
+        0.1, 
+        0.1,
+        0.1
+    );
+
+    pop.set_crossing_probability(0.8);
+
+
+
+    int nsteps=1000;
     cout << "Evolution started\n";
     for(int j=0; j<nsteps; j++)
     {
         //cout << "evolution "<<j<<endl;
         pop.evolve();
         pop.save_population_best();
+        pop.save_avg_cost();
         progress(j, nsteps);
     }
     cout<<RESET<<endl;
